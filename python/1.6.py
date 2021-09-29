da2a355debf9f181548c5e92b09de3955f941a9b
DmitryMorozov0 <90560892+DmitryMorozov0@users.noreply.github.com>
2021-09-29 17:59:28 +0300

    Add files via upload

---
+++ python/1.6.py
@@ -0,0 +1,5 @@
+# -- coding: utf-8 --
+x=input("Введите число: ")
+y=input("Введите число: ")
+x,y = y,x
+print(x,y)
---
+++ python/1.7.py
@@ -0,0 +1,7 @@
+# -- coding: utf-8 --
+number=int((input("Введите число: ")))
+i=number%2
+if i==0:
+    print("Четное")
+else:
+    print("Нечетное")
