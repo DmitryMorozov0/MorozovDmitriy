afa54a746201b6ef5b59a2e6dcc99db303363add
DmitryMorozov0 <90560892+DmitryMorozov0@users.noreply.github.com>
2021-09-29 17:50:50 +0300

    Delete питон directory

--- питон/1.4.py
+++
@@ -1,7 +0,0 @@
-# -- coding: utf-8 --
-seconds=int(input("Введите некотоое кол-во сек: "))
-sec=seconds%86400%3600%60
-min=seconds%86400//60
-h=seconds%86400//24
-d=seconds//86400
-print(d, ":", h, ":", min, ":", sec)
